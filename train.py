5590bce2b48d214410991644c1b1b195bddce512
Hao <haowang@ece.utoronto.ca>
2020-07-30 22:31:07 -0400

    implement RL

--- train.py
+++ train.py
@@ -7,12 +7,14 @@ import client
 import config
 import server
 from rl.env import Environment
-from rl.agent import Agent
+from rl.agent import MLP, ActorCritic
 
 import torch
-
-
-MAX_EPISODES = 1000
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.distributions as distributions
+import numpy as np
 
 
 # Set up parser
@@ -24,13 +26,59 @@ parser.add_argument('-l', '--log', type=str, default='INFO',
 
 args = parser.parse_args()
 
+def calculate_advantages(returns, values, normalize = True):
+    advantages = returns - values
+    if normalize:        
+        advantages = (advantages - advantages.mean()) / advantages.std()        
+    return advantages
 
-def calculate_reward():
 
-    pass
+def update_policy(
+    policy, states, actions, log_prob_actions, \
+        advantages, returns, optimizer, ppo_steps, ppo_clip):
+    
+    total_policy_loss = 0 
+    total_value_loss = 0
+    
+    advantages = advantages.detach()
+    log_prob_actions = log_prob_actions.detach()
+    actions = actions.detach()
+    
+    for _ in range(ppo_steps):
+                
+        #get new log prob of actions for all input states
+        action_pred, value_pred = policy(states)
+        value_pred = value_pred.squeeze(-1)
+        action_prob = F.softmax(action_pred, dim = -1)
+        dist = distributions.Categorical(action_prob)
+        
+        #new log prob using old actions
+        new_log_prob_actions = dist.log_prob(actions)
+        
+        policy_ratio = (new_log_prob_actions - log_prob_actions).exp()
+                
+        policy_loss_1 = policy_ratio * advantages
+        policy_loss_2 = torch.clamp(
+            policy_ratio, min = 1.0 - ppo_clip, max = 1.0 + ppo_clip) * advantages
+        
+        policy_loss = - torch.min(policy_loss_1, policy_loss_2).sum()
+        
+        value_loss = F.smooth_l1_loss(returns, value_pred).sum()
+    
+        optimizer.zero_grad()
+
+        policy_loss.backward()
+        value_loss.backward()
+
+        optimizer.step()
+    
+        total_policy_loss += policy_loss.item()
+        total_value_loss += value_loss.item()
+    
+    return total_policy_loss / ppo_steps, total_value_loss / ppo_steps
 
 
-def fl_train():
+def fl_train(policy):
 
     states = []
     actions = []
@@ -66,11 +114,16 @@ def fl_train():
                 for client in sample_clients]
         states.append(state)
 
-        prune_level_pred, value_pred = make_action(state)
+        prune_level_pred, value_pred = policy(state)
+
+        action_prob = F.softmax(action_pred, dim = -1)
+        dist = distributions.Categorical(action_prob)
+        action = dist.sample()
+
 
         # one round FL training
         accuracy = rl_server.round(
-            round_id, prune_level, sample_clients, level_accuracy_per_client)
+            round_id, action, sample_clients, level_accuracy_per_client)
 
         logging.info(f'Round {round_id} accuracy: {accuracy}')
         logging.info(f'Round {round_id}: {level_accuracy_per_client}')
@@ -90,12 +143,63 @@ def fl_train():
 
     return episode_reward, 
 
-def main():
 
-    agent = Agent()
+
+
+def main():
+    MAX_EPISODES = 500
+    DISCOUNT_FACTOR = 0.99
+    N_TRIALS = 25
+    REWARD_THRESHOLD = 475
+    PRINT_EVERY = 10
+    PPO_STEPS = 5
+    PPO_CLIP = 0.2
+
+
+    INPUT_DIM = 30
+    HIDDEN_DIM = 128
+    OUTPUT_DIM = 3
+
+    actor = MLP(INPUT_DIM, HIDDEN_DIM, OUTPUT_DIM)
+    critic = MLP(INPUT_DIM, HIDDEN_DIM, 1)
+
+    policy = ActorCritic(actor, critic)
+
+    LEARNING_RATE = 0.01
+    optimizer = optim.Adam(policy.parameters(), lr = LEARNING_RATE)
+
+    def init_weights(m):
+        if type(m) == nn.Linear:
+            torch.nn.init.xavier_normal_(m.weight)
+            m.bias.data.fill_(0)
     
-    for episode in range(0, 1):
-        fl_train()
+    policy.apply(init_weights)
+
+    train_rewards = []
+    test_rewards = []
+
+    for episode in range(1, MAX_EPISODES+1):
+        
+        policy_loss, value_loss, train_reward = fl_train(
+            policy, optimizer, DISCOUNT_FACTOR, PPO_STEPS, PPO_CLIP)
+        
+        test_reward = evaluate(test_env, policy)
+        
+        train_rewards.append(train_reward)
+        test_rewards.append(test_reward)
+        
+        mean_train_rewards = np.mean(train_rewards[-N_TRIALS:])
+        mean_test_rewards = np.mean(test_rewards[-N_TRIALS:])
+        
+        if episode % PRINT_EVERY == 0:
+        
+            print(f'| Episode: {episode:3} | Mean Train Rewards: {mean_train_rewards:5.1f} | Mean Test Rewards: {mean_test_rewards:5.1f} |')
+        
+        if mean_test_rewards >= REWARD_THRESHOLD:
+            
+            print(f'Reached reward threshold in {episode} episodes')
+            
+            break
  
 
 if __name__ == "__main__":
