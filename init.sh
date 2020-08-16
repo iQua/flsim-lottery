3047ae50b054476dc84c6c08b78105a53ef1fb02
Hao <haowang@ece.utoronto.ca>
2020-08-16 10:58:04 -0400

    update init.sh; fix pool client number

--- init.sh
+++ init.sh
@@ -6,10 +6,11 @@ then
     mkdir ./logs
 fi
 
-chown ubuntu:ubuntu -R /mnt
 
 if [ ! -d /mnt/open_lth_data ]
 then
     echo "Create directory /mnt/open_lth_data"
     mkdir /mnt/open_lth_data
 fi
+
+chown ubuntu:ubuntu -R /mnt
