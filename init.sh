975da8a0a77c7bee564f0cf4d6f065b2ed6ade24
Hao <haowang@ece.utoronto.ca>
2020-08-16 10:45:51 -0400

    update init.sh

--- init.sh
+++ init.sh
@@ -5,3 +5,11 @@ then
     echo "Create directory ./logs"
     mkdir ./logs
 fi
+
+chown ubuntu:ubuntu -R /mnt
+
+if [ ! -d /mnt/open_lth_data ]
+then
+    echo "Create directory /mnt/open_lth_data"
+    mkdir /mnt/open_lth_data
+fi
